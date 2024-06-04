5713f8e40364a27ac534f527c8e27a2110c4864a
00012121 <wiut00012121@gmail.com>
2024-06-04 10:47:41 +0500

    Getting the books controller

--- BookStoreFront/Controllers/BookController.cs
+++ BookStoreFront/Controllers/BookController.cs
@@ -9,5 +9,27 @@ namespace BookStoreFront.Controllers
     public class BookController : Controller
     {
         private const string BaseApi = "https://localhost:7172/";
+        
+        // GET: BookController
+        public async Task<ActionResult> Index()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new System.Uri(BaseApi);
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await httpClient.GetAsync("api/Book");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read the content of the response and deserialize it into a list of Books
+                    var content = await response.Content.ReadAsStringAsync();
+                    var books = JsonConvert.DeserializeObject<IEnumerable<Book>>(content);
+                    return View(books);
+                }
+                return View();
+            }
+
+        }
     }
 }
